c0df11b8058e2747fcf1cb42470952d581b3f9f0
datnguyen <dat.nguyentien.set13@eiu.edu.vn>
2018-11-26 09:51:19 +0700

    fix app.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Login from './components/Login'
 
 class App extends Component {
+  state ={
+    login: false
+  }
+
+  setLoginState = (login) => {
+    this.setState({login})
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    return(
+      <div>
+        {!this.state.login?<Login loginState={this.setLoginState}/>:<div>aaaa</div>}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+
+
+export default App
